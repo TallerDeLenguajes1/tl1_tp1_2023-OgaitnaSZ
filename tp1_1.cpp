dc0570c7d6a082d6c92773272cac2022c145fc7c
Santiago Z <test@gmail.com>
2023-03-23 21:40:09 -0300

    Punto E

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+    int a = 10;
 
-void main(){
+    int *p = &a;
+
+int main(){
     printf("Hola mundo");
 
+    printf("\nEl contenido del puntero es: %d", *p);
+    printf("\nLa direccion de memoria almacenada en el puntero es: %d", p);
+    printf("\nLa direccion de memoria del puntero es: %d", &p);
 
+    printf("\nEl tamaño de memoria utilizado es: %d", sizeof(*p));
+
+    return 0;
 }
